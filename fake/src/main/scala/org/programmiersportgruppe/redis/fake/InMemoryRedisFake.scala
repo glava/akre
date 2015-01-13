fa1a7ddf73d1dd3bb44d9ec29bba246dd43e3d01
Goran Ojkic <goran.ojkic@here.com>
2015-01-13 17:59:12 +0100

    work on fakes

--- fake/src/main/scala/org/programmiersportgruppe/redis/fake/InMemoryRedisFake.scala
+++ fake/src/main/scala/org/programmiersportgruppe/redis/fake/InMemoryRedisFake.scala
@@ -4,16 +4,17 @@ import akka.util.ByteString
 
 import org.programmiersportgruppe.redis._
 import org.programmiersportgruppe.redis.commands._
+import org.programmiersportgruppe.redis.protocol.RValueParser
 
 
 class InMemoryRedisFake {
 
-  var value: ByteString = null
+  var db: Map[Key, ByteString] = Map.empty
 
   def execute(command: RecognisedCommand): RValue = command match {
-    case GET(key) => RBulkString(value)
-    case SET(_, value, _, _) =>
-      this.value = value
+    case GET(key) => RBulkString(db(key))
+    case SET(key, value, _, _) =>
+      db = db.updated(key, value)
       RSimpleString.OK
     case c => RError("I have no clue what this command is! " + c)
   }
--- fake/src/main/scala/org/programmiersportgruppe/redis/fake/RedisServerFake.scala
+++ fake/src/main/scala/org/programmiersportgruppe/redis/fake/RedisServerFake.scala
@@ -7,53 +7,60 @@ import akka.io.{IO, Tcp}
 import org.programmiersportgruppe.redis.commands.RecognisedCommand
 import org.programmiersportgruppe.redis.{UntypedCommand, RArray}
 import org.programmiersportgruppe.redis.protocol.RValueParser
+import org.programmiersportgruppe.redis.RValue
 
 
 object RedisServerFake {
-    def props(): Props = Props(classOf[RedisServerFake])
+  def props(): Props = Props(classOf[RedisServerFake])
 }
 
 class RedisServerFake extends Actor {
 
-    import Tcp._
-    import context.system
-    //TODO: please dont bound this like this
-    IO(Tcp) ! Bind(self, new InetSocketAddress("127.0.0.1", 4321))
+  import Tcp._
+  import context.system
 
-    def receive = {
-        case b @ Bound(localAddress) =>
-        // do some logging or setup ...
-            println("bound")
+  //TODO: please dont bound this like this
+  IO(Tcp) ! Bind(self, new InetSocketAddress("127.0.0.1", 4321))
+  val inMemoryRedis = new InMemoryRedisFake
 
-        case CommandFailed(_: Bind) => context stop self
+  def receive = {
+    case b@Bound(localAddress) =>
+      // do some logging or setup ...
+      println("bound")
 
-        case c @ Connected(remote, local) =>
-            val handler = context.actorOf(Props[CommandHandler])
-            val connection = sender()
-            connection ! Register(handler)
-    }
+    case CommandFailed(_: Bind) => context stop self
+
+    case c@Connected(remote, local) =>
+      val handler = context.actorOf(CommandHandler.props(inMemoryRedis))
+      val connection = sender()
+      connection ! Register(handler)
+  }
 
 }
 
-class CommandHandler extends Actor {
-    import Tcp._
-    //TODO: this is not correct place for this
-    val inMemoryRedis = new InMemoryRedisFake
-    def receive = {
-        case Received(data) => {
-            RValueParser.parseNonEmpty(data).map {
-                    case RArray(cont) => UntypedCommand.fromRValue(cont).flatMap(execute)
-                    case _ => None
-            }.map { result =>
-                sender() ! Write(result.get)
-            }
+class CommandHandler(inMemoryRedis: InMemoryRedisFake) extends Actor {
 
-        }
-        case PeerClosed     => context stop self
-    }
+  import Tcp._
 
-    private def execute(uc: UntypedCommand) = {
-        RecognisedCommand.fromUntypedCommand(uc).map(inMemoryRedis.execute)
-    }
+  def receive = {
+    case Received(data) =>
+      RValueParser.parseNonEmpty(data).map {
+        case RArray(cont) => UntypedCommand.fromRValue(cont).flatMap(execute)
+        case _ => None
+      }.map { result =>
+        println(s"sending ${RValue.rValue2ByteString(result.get)}")
+        sender() ! RValue.rValue2ByteString(result.get)
+      }
 
+    case PeerClosed => context stop self
+  }
+
+  private def execute(uc: UntypedCommand) = {
+    RecognisedCommand.fromUntypedCommand(uc).map(inMemoryRedis.execute)
+  }
+
+}
+
+object CommandHandler {
+  def props(inMemoryRedis: InMemoryRedisFake) = Props(new CommandHandler(inMemoryRedis))
 }
